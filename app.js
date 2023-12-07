dee13da6d847ca4748ab64b006ec422419e359eb
alissonfandre <alissonfeliciano@gmail.com>
2023-12-06 21:49:21 -0300

    bugs fixed

--- app.js
+++ app.js
@@ -3,28 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-//import do sequelize
 const { Sequelize } = require('sequelize');
-
-//criando a constante sequelize e passando as informações
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const sequelize = require('./db');
-
-sequelize.sync();
-
-//models importaçoes
 const Tarefa = require('./models/tarefa');
-
- const usuario = require('./models/usuario');
-
-
+const usuario = require('./models/usuario');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tarefasRouter = require('./routes/tarefasRouter');
 var usuariosRouter = require('./routes/usuariosRouter');
 
-
 var app = express();
+
+// Middleware para permitir requisições CORS
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,7 +33,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tarefas', tarefasRouter);
 app.use('/usuarios', usuariosRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
